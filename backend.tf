f078e14297f8d2eec61f1d2872d1bd56726bc66d
Nasramohammed <105649343+Nasramohammed@users.noreply.github.com>
2024-02-26 16:11:43 +0000

    Add files via upload
    changed backend and added sandox account

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3"{
-        bucket = "wordpress-state-store"
+        bucket = "wordpress-state-store-1"
         key = "network/terraform.tfstate"
         region = "eu-west-1"
     }
